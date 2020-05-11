0e04fe97d62bd77d446f1e645bee311c03ec6220
PARDEEP KAUR <pkaur84@aol.com>
2020-05-11 00:58:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,11 +3,12 @@ array.include?(element)
 end
 
 def using_sort(array)
-array.sort?(array)
+sorted_array = array.sort
+  sorted_array
 end
 
 def using_reverse(array)
-array.reverse?(array)
+array.reverse
 end
 
 def using_first(array)
